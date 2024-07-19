2ecdf18f3a1a2d76ad08b37c2f76d9e49a5e5235
Van Truong <truongtran.lv2019@gmail.com>
2024-07-19 22:03:48 +0700

    update api internal

--- notification-service/src/main/java/notificationservice/client/UserClient.java
+++ notification-service/src/main/java/notificationservice/client/UserClient.java
@@ -4,11 +4,12 @@ import notificationservice.common.CommonResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @FeignClient(name = "identity-service", url = "http://localhost:9001/internal/identity")
 public interface UserClient {
-  @GetMapping("/users/existed")
+  @GetMapping("/user/existed")
   ResponseEntity<CommonResponse<Object>> existedUserByEmail(@RequestParam("email") String email);
 }
